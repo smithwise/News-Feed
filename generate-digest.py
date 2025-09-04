8b4f0c3e1ba2f8462dbea8bb2df44cb939440d78
smithwise <smithwise.secure@gmail.com>
2025-09-04 12:35:18 -0500

    Update generate-digest.py
    Added Try ... Except for input files, should generate empty CSVs if the sources in scope are not found
    
    added in OS pathing using pathlib, works on Windows / Linux, as long as directory hierarchy is in place:
    
    input files should be at: ../'archives'
    output will go to: ../'digests'

--- generate-digest.py
+++ generate-digest.py
@@ -1,43 +1,61 @@
-# step 1 - receive csv inputs (headline & link)
 from datetime import date, timedelta  #identifying last week of files via REGEX
 import csv #reading archives and creating digest files
+import os
+from pathlib import Path
 
 today = date.today()
 date_scope = []
 digest_dict = {}
+log_content = ''
 
-for i in range(7):  #identify relevant dates for digest
-    date_scope.append(str(today - timedelta(i))) #tuck date objects as string into list
+# path variables for I/O file management
+cwd = Path.cwd()
+home_dir = cwd.parent
+input_dir = home_dir / 'archives'
+output_dir = home_dir / 'digests'
 
-''' for testing only, leave commented out
+
+# identify relevant dates for digest; MAY CHANGE THIS
+#
+# If changed, would target all files matching name template in source folder
+# (this would be reliant on cleanup operations moving old files once processed)
+for i in range(7):
+    date_scope.append(str(today - timedelta(i)))
+
+# testing for missing files, generates empty one and logs if so.
 for date in date_scope:
-    date_name="BC-"+date+".csv"
-    with open(date_name, "w", newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['title','link'])
-        writer.writerow([f'{date_name} title',f'{date_name} link'])
-'''
+    date_name = "BC-" + date + ".csv"
+    try:
+        for date in date_scope:  # could change to for file in source_dir:
+            target_name = "BC-"+date+".csv"
+            target_file = input_dir / target_name
+            # search file name as template using date
+            with open(target_file, "r", newline='') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    row["last update"] = date
+                    digest_dict[row["title"]] = row
+    except:
+        with open(target_file, "w", newline='',  encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['title','link'])
+            writer.writerow([f'{date_name} no file found, generated empty file','n/a'])
 
-# step 2 - write inputs to dictionary for dedup
-for date in date_scope:
-    with open("BC-"+date+".csv", "r", newline='') as file: #search file name as template using date
-        reader = csv.DictReader(file)
-        for row in reader:
-            #digest_dict.update() #write contents of file to dictionary
-            row["last update"] = date  # add/update the field inside the row
-            digest_dict[row["title"]] = row  # store row keyed by title
-
+# taking contents of source files and generating new CSV with the cumulation
 filename = f"Digest-{today}.csv"
-with open(filename, mode="w",  newline='', encoding='utf-8') as file:
+target_out = output_dir / filename
+with open(target_out, mode="w",  newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=["title", "link", "last update"])
     writer.writeheader()
     writer.writerows(digest_dict.values())
 
+print(f"cwd={cwd} \n home={home_dir} \n input={input_dir} \n output={output_dir}")
 ''' Misc needs
 #### need path awareness - source files, script, and output are all in separate locations
 
 # error logging
-## (try...except) logic needed to log errors (if possible) for final script since this is running without oversight
+## (try...except) logic needed to log errors (if possible) for final script since... 
+   ## this is running without oversight
 ## perhaps even including the error message in the digest email for the week that fails to run
 '''
 
@@ -49,14 +67,16 @@ with open(filename, mode="w",  newline='', encoding='utf-8') as file:
 ''' Email generation needs
 '''
 # step 3 - draft .msg with UL of hyperlinked headlines
+
+
 # step 4 - smtp .msg to smithwise.secure@gmail.com
 
 
 ''' GIT Commit comment drafting
-Halfway done, added in (commented out) section to generate missing files for testing. Not needed in final product as files should arealdy be extant
-
-Working on path awareness, need to search for source files in another directory and out put files into another directory rather than the script directory.
-- WIP move scoped archive files into a new directory when done
+first half draft of the Digest creation script. 
+Identifies the (BC-<date>.csv) archive files in scope, writes their contents to new dictionary
+- WIP creates new CSV from new dictionary
+- WIP moves scoped archive files into a new directory 
 - SHOULD DO, run a temporary script on Pi to create an initial digest containing all previous articals
 - WIP add cron job to run digest script weekly on Tuesdays at 0600
 '''
